27639308ff8a7aa0021a010ebc298f94d92fb3ac
Murod Akhmedov <murod.london@gmail.com>
2024-06-20 12:51:50 +0100

    Initial commit

---
+++ src/data/data.ts
@@ -0,0 +1,15 @@
+import { nanoid } from "nanoid"
+
+export const notes = [{
+    id: nanoid(),
+    title: 'Note 1',
+    content: 'Content 1',
+    created: new Date(),
+    updated: new Date(),
+}, {
+    id: nanoid(),
+    title: 'Note 2',
+    content: 'Content 2',
+    created: new Date(),
+    updated: new Date(),
+}]
